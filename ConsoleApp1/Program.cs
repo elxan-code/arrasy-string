5d110e983da78f01f3f62a9cb8b1d00f3b69c93f
unknown <you@example.com>
2021-11-02 06:48:50 +0400

    arrays

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string daxil = "Rasim is a mascline given name of arabic origin maning planer, architect.";      //sekl 3
+            //Console.WriteLine(daxil.Substring(0, 50)+"...Read more");
+
+
+
+            // int[] arr = { 23, 75, 89, 12, 45, 78 };
+            // int Sum = sum(arr);
+            // Console.WriteLine(Sum);
+            //    int[] arr = { 23, 75, 89, 12, 45, 78 };
+            //    Console.WriteLine(dogru(arr, 75));
+        }
+        // public static int sum(int[] arr)
+        //  {
+        //     int c = 0;
+        //    for (int i = 0; i < arr.Length; i++)
+        //   {
+        //        c += arr[i];
+        //   }
+        //   return c;
+        //   }
+        // static bool dogru(int[] arr, int num)
+        //  {
+        //    for (int i = 0; i < arr.Length; i++)
+        //   {
+        //       if (arr[i] == num)
+        //           return true;
+        //   }
+        //    return false;
+        // }
+
+    }
+}
